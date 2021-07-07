2c67cbcb0f500b170b155c3304e82ab5c3de3e0b
Yiwen Wang <yiwenwa@amazon.com>
2021-07-07 14:50:27 -0700

    Add envVar SUDO_USER

--- pyramid_bullet/views.py
+++ pyramid_bullet/views.py
@@ -4,4 +4,4 @@ import os
 
 @view_config(route_name='home', renderer='templates/mytemplate.jinja2')
 def my_view(request):
-    return {'project': 'pyramid_bullet', 'env': os.environ.get('FOO')}
+    return {'project': 'pyramid_bullet', 'env': os.environ.get('SUDO_USER')}
